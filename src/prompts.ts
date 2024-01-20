fe22bf12495f638d3a8c97bde45a161b2453da31
Jonathan Atiene <34762800+bemijonathan@users.noreply.github.com>
2024-01-21 00:12:00 +0100

    remove langchain

--- src/prompts.ts
+++ src/prompts.ts
@@ -5,27 +5,24 @@ Your summary should include an analysis of the changes made in the git diff, hig
 Be specific and descriptive, accurately identifying the affected files and lines of code. 
 Present your summary in a clear and concise manner, ensuring readability and comprehension for all stakeholders involved in the code review process. 
 -----------------------------
-{diff}
 `
 
 export const jiraPrompt = `
 you are given a jira ticket that contains the acceptance criteria for the task you are reviewing 
 the aim is to collect all possible information from the jira ticket 
-and create a summary of all the acceptance criteria and and todos from from the description
+and create a list of summary of all the acceptance criteria and and todos from from the description
 
 Jira Ticket description
 ______________________________________
- {ticketDescription}
 `
 
 export const acSummariesPrompt = `
-Your job is to take the code changes from the repository and the jira ticket 
-you will take the acceptance criteria from the 
-ticket and create a readme checklist of the acceptance criteria
+Your job is to use the summary of the code changes and the summary of a jira ticket 
+ticket and create a readme checklist of the acceptance criteria from the summary of the jira ticket
 you will then compare the code changes to the acceptance criteria 
 if the code changes meet the acceptance criteria you will check the box 
 
-you will use this format 
+you will use this format example: 
 
 ### Task: move all business logic to the services folder  
 
@@ -39,4 +36,3 @@ if all the criteria is met you will check the box
 - [x] add a new validated dropdown column to chenge the status and the unmet criteria will be left unchecked
 - [ ] implement the correct logic to change the status
 `
-export const generatePrompt = () => {}
